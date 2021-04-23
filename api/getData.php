a39e98bd50797e679ba08bb40738b909941437a2
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-23 17:03:20 -0300

    first commit

---
+++ api/getData.php
@@ -0,0 +1,37 @@
+<?php
+    require '../db_config.php';
+
+    $num_rec_per_page = 5;
+
+    if (isset($_GET["page"])) { $page = $_GET["page"]; } else { $page=1;};
+        # code...
+        $start_from = ($page -1) * $num_rec_per_page;
+
+        if (!empty($_GET["search"])) {
+            # code...
+            $sqlTotal = "SELECT * FROM items
+                        WHERE (title LIKE '%'".$_GET["search"]."%'
+                        OR description LIKE '%".$_GET["search"]."%')";
+            $sql = "SELECT * FROM items 
+                        WHERE (title LIKE '%".$_GET["search"]."%' 
+                        OR description LIKE '%".$_GET["search"]."%') 
+                        LIMIT $start_from, $num_rec_per_page";             
+        } else {
+            # code...
+            $sqlTotal = "SELECT * FROM items"; 
+            $sql = "SELECT * FROM items LIMIT $start_from, $num_rec_per_page";
+        }
+        $result = $mysqli->query($sql);
+
+        while ($row = $result->fetch_assoc()) {
+            # code...
+            $json[] = $row;
+        }
+        $data['data'] = $json;
+        
+    $result = mysqli_query($mysqli,$sqlTotal);
+    $data['total'] = mysqli_num_rows($result);
+
+    echo json_encode($data);
+
+?>
